b5803b8462ec56841412a59c16f3fb85b4dd7879
kiiooxx <kiiooxx09@gmail.com>
2024-07-15 14:27:55 +0900

    게임오브젝트 물리와 충돌
    Rigidbody2D
    Collider2D

--- Study/Assets/Movement2D.cs
+++ Study/Assets/Movement2D.cs
@@ -4,6 +4,15 @@ public class Movement2D : MonoBehaviour
 {
     private float   moveSpeed = 5.0f;               //이동 속도
     private Vector3 moveDirection = Vector3.zero;   //이동 방향
+    private Rigidbody2D rigid2D;
+
+
+    private void Awake()
+    {
+        //게임오브젝트의 컴포넌트에 접근하는 방법 GetComponent<컴포넌트 이름>();
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
 
     private void Update()
     {
@@ -17,11 +26,25 @@ public class Movement2D : MonoBehaviour
         //Non : 0
         float y = Input.GetAxisRaw("Vertical");     //위아래 이동
 
+
+        //좌우 이동 시 이미지 반전
+        if(x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        //Rigidbody2D 컴포넌트에 있는 속력(velocity) 변수 설정
+        rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
+
         //이동방향 설정
-        moveDirection = new Vector3(x, y, 0);
+        //moveDirection = new Vector3(x, y, 0);
 
         //새로운 위치 = 현재 위치 + (방향 * 속도)
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        //transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
 
         //새로운 위치 = 현재 위치 + (방향 * 속도)
@@ -32,5 +55,8 @@ public class Movement2D : MonoBehaviour
         //위 코드와 같은 결과
 
         //transform.position += Vector3.right * 1 * Time.deltaTime;
+
+
+
     }
 }
