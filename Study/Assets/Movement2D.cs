65d85f5ac38abd9f899aa6f756dff5370716b43b
kiiooxx <kiiooxx09@gmail.com>
2024-07-20 10:27:10 +0900

    Tilemap

--- Study/Assets/Movement2D.cs
+++ Study/Assets/Movement2D.cs
@@ -31,35 +31,35 @@ public class Movement2D : MonoBehaviour
         capsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
 
-    public void FixedUpdate()
-    {
-        //플레이어 오브젝트의 Collider2D min, center, max 위치 정보
-        Bounds bounds = capsuleCollider2D.bounds;
-        //플레이어의 발 위치 설정
-        footPosition = new Vector2(bounds.center.x, bounds.min.y);
-        //플레이어의 발 위치에 원을 생성하고, 원이 바닥과 닿아있으면 isGrounded = true
-        isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayer);
+    //public void FixedUpdate()
+    //{
+    //    //플레이어 오브젝트의 Collider2D min, center, max 위치 정보
+    //    Bounds bounds = capsuleCollider2D.bounds;
+    //    //플레이어의 발 위치 설정
+    //    footPosition = new Vector2(bounds.center.x, bounds.min.y);
+    //    //플레이어의 발 위치에 원을 생성하고, 원이 바닥과 닿아있으면 isGrounded = true
+    //    isGrounded = Physics2D.OverlapCircle(footPosition, 0.1f, groundLayer);
 
-        //플레이어의 발이 땅에 닿아 있고, y축 속도가 0이하이면 점프 횟수 초기화
-        //velocity.y <= 0을 추가하지 않으면 점프키를 누르는 순간에도 초기화가 되어
-        //최대 점프 횟수를 2로 설정하면 3번까지 점프가 가능하게 된다
-        if(isGrounded == true && rigid2D.velocity.y <= 0)
-        {
-            currentJumpCount = maxJumpCount;
-        }
+    //    //플레이어의 발이 땅에 닿아 있고, y축 속도가 0이하이면 점프 횟수 초기화
+    //    //velocity.y <= 0을 추가하지 않으면 점프키를 누르는 순간에도 초기화가 되어
+    //    //최대 점프 횟수를 2로 설정하면 3번까지 점프가 가능하게 된다
+    //    if(isGrounded == true && rigid2D.velocity.y <= 0)
+    //    {
+    //        currentJumpCount = maxJumpCount;
+    //    }
 
 
-        //낮은 점프, 높은 점프 구현을 위한 중력 계수(gravityScale) 조절 (Jump up일 때만 적용)
-        //중력 계수가 낮은 if 문은 높은 점프가 되고, 중력 계수가 높은 else 문은 낮은 점프가 된다
-        if (isLongJump && rigid2D.velocity.y > 0)
-        {
-            rigid2D.gravityScale = 1.0f;
-        }
-        else
-        {
-            rigid2D.gravityScale = 2.5f;
-        }
-    }
+    //    //낮은 점프, 높은 점프 구현을 위한 중력 계수(gravityScale) 조절 (Jump up일 때만 적용)
+    //    //중력 계수가 낮은 if 문은 높은 점프가 되고, 중력 계수가 높은 else 문은 낮은 점프가 된다
+    //    if (isLongJump && rigid2D.velocity.y > 0)
+    //    {
+    //        rigid2D.gravityScale = 1.0f;
+    //    }
+    //    else
+    //    {
+    //        rigid2D.gravityScale = 2.5f;
+    //    }
+    //}
 
     public void Move(float x)
     {
@@ -87,49 +87,49 @@ public class Movement2D : MonoBehaviour
     }
 
 
-    //private void Update()
-    //{
-    //    //Input 클래스 : PC, 모바일 등의 입력 메소드, 마우스 좌표와 같은 각종 프로퍼티 정보를 제공
-    //    //Negative left, a : -1
-    //    //Positive right, d : 1
-    //    //Non : 0
-    //    float x = Input.GetAxisRaw("Horizontal");   //좌우 이동       
-    //    //Negative down, s : -1
-    //    //Negative up, w : 1
-    //    //Non : 0
-    //    float y = Input.GetAxisRaw("Vertical");     //위아래 이동
+    private void Update()
+    {
+        //Input 클래스 : PC, 모바일 등의 입력 메소드, 마우스 좌표와 같은 각종 프로퍼티 정보를 제공
+        //Negative left, a : -1
+        //Positive right, d : 1
+        //Non : 0
+        float x = Input.GetAxisRaw("Horizontal");   //좌우 이동       
+        //Negative down, s : -1
+        //Negative up, w : 1
+        //Non : 0
+        float y = Input.GetAxisRaw("Vertical");     //위아래 이동
 
 
-    //    //좌우 이동 시 이미지 반전
-    //    if(x > 0)
-    //    {
-    //        transform.localScale = new Vector3(-1, 1, 1);
-    //    }
-    //    else if (x < 0)
-    //    {
-    //        transform.localScale = new Vector3(1, 1, 1);
-    //    }
+        //좌우 이동 시 이미지 반전
+        if (x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
 
-    //    //Rigidbody2D 컴포넌트에 있는 속력(velocity) 변수 설정
-    //    rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
+        //Rigidbody2D 컴포넌트에 있는 속력(velocity) 변수 설정
+        rigid2D.velocity = new Vector3(x, y, 0) * speed;
 
-    //    //이동방향 설정
-    //    //moveDirection = new Vector3(x, y, 0);
+        //이동방향 설정
+        //moveDirection = new Vector3(x, y, 0);
 
-    //    //새로운 위치 = 현재 위치 + (방향 * 속도)
-    //    //transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-
-    //    //새로운 위치 = 현재 위치 + (방향 * 속도)
-    //    //(1, 0, 0) => 1 : 오른쪽으로 이동
-    //    //transform.position = transform.position + new Vector3(1, 0, 0) * 1;
-
-    //    //transform.position += Vector3.right * 1;
-    //    //위 코드와 같은 결과
-
-    //    //transform.position += Vector3.right * 1 * Time.deltaTime;
+        //새로운 위치 = 현재 위치 + (방향 * 속도)
+        //transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
 
+        //새로운 위치 = 현재 위치 + (방향 * 속도)
+        //(1, 0, 0) => 1 : 오른쪽으로 이동
+        //transform.position = transform.position + new Vector3(1, 0, 0) * 1;
 
-    //}
+        //transform.position += Vector3.right * 1;
+        //위 코드와 같은 결과
+
+        //transform.position += Vector3.right * 1 * Time.deltaTime;
+
+
+
+    }
 }
